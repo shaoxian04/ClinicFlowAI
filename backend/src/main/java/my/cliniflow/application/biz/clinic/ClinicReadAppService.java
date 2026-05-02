21d9756d3d7561a0ac1f7fa1578e0b3744075f2d
shaoxian04 <shaoxian04@gmail.com>
2026-05-02 13:47:40 +0800

    feat(clinic): GET /api/clinic + ClinicReadAppService

---
+++ backend/src/main/java/my/cliniflow/application/biz/clinic/ClinicReadAppService.java
@@ -0,0 +1,25 @@
+package my.cliniflow.application.biz.clinic;
+
+import my.cliniflow.domain.biz.clinic.info.ClinicInfo;
+import my.cliniflow.infrastructure.config.ClinicProperties;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClinicReadAppService {
+    private final ClinicProperties props;
+
+    public ClinicReadAppService(ClinicProperties props) {
+        this.props = props;
+    }
+
+    public ClinicInfo get() {
+        return new ClinicInfo(
+                props.name(),
+                props.addressLine1(),
+                props.addressLine2(),
+                props.phone(),
+                props.email(),
+                props.registrationNumber()
+        );
+    }
+}
